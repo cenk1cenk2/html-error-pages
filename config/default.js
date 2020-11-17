89edd46d7f2c8b575785513cc56ffba2d521dc43
semantic-release <cenk1cenk2cenk3@gmail.com>
2020-11-17 19:16:25 +0000

    chore(release): 2.0.11 - 20201117 [skip ci]
    ## [2.0.11](https://github.com/cenk1cenk2/html-error-pages/compare/v2.0.10...v2.0.11) (2020-11-17)
    
    ### Bug Fixes
    
    * **deps:** update dependency @cenk1cenk2/react-template-components to ^1.3.1 ([714ce83](https://github.com/cenk1cenk2/html-error-pages/commit/714ce83649be4ac07cdccf6ba4be730db1d9fbc3))

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ const package = readPkgUp.sync()
 module.exports = {
   package: {
     name: package.packageJson.name,
-    version: '2.0.10'
+    version: '2.0.11'
   },
   mainPageUrl: 'https://kilic.dev'
 }
